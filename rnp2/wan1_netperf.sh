925a447480f0b7e565bc33bb660cbdcb4d1464b8
JannikHAW <jannik.iacobi@haw-hamburg.de>
2014-12-15 20:27:22 +0100

    protokoll half ready (comments needed)

--- rnp2/wan1_netperf.sh
+++ rnp2/wan1_netperf.sh
@@ -7,6 +7,6 @@ echo "Windowsize Transfer Rate (mbit/s)"
 # test multiple connections that end directly
 for (( i=512;$i<=600000;i*=2 )); 
 do
-	netperf -H 192.168.18.136 -l 1 -- -S $i | awk '/16384 /{printf "%9d  %6.2f\n", $1/2, $5}'
+	netperf -H 192.168.18.133 -l 1 -- -S $i | awk '/16384 /{printf "%9d  %6.2f\n", $1/2, $5}'
 done
 exit $?
--- rnp2/wan1_ping.sh
+++ rnp2/wan1_ping.sh
@@ -12,17 +12,17 @@ usage()
 #    exit 1
 #fi
 
-rm -f 3_1_1.dat
+rm -f 3_3_1.dat
 # test multiple connections that end directly
 for (( i=50;$i<=1500;i+=100 ))
 do
     echo $i
-    ping 192.168.18.136 -s $i -c 5 >>3_1_1.dat
+    ping 192.168.18.133 -s $i -c 5 >>3_3_1.dat
 done
-awk '/time=/{print $1, substr($7,6)}' 3_1_1.dat >3_1_2.dat
+awk '/time=/{print $1, substr($7,6)}' 3_3_1.dat >3_3_2.dat
 
 gnuplot -p -e "f(x)=a+b*x;
-fit f(x) \"3_1_2.dat\" via a,b;
-plot \"3_1_2.dat\", f(x);"
+fit f(x) \"3_3_2.dat\" via a,b;
+plot \"3_3_2.dat\", f(x);"
 
 exit $?
